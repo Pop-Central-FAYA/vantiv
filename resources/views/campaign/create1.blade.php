9bee82df335529f5bade17725e8f208338a61a0a
rheedwahn <rabusari@student.lautech.edu.ng>
2017-11-08 17:38:46 +0100

    corrected
    Former-commit-id: b0680a50af32b4d6377ad76f3a8e31c6a08f7db0

--- resources/views/campaign/create1.blade.php
+++ resources/views/campaign/create1.blade.php
@@ -1,75 +1,75 @@
-@extends('app.layout')
+@extends('layouts.app')
 
 @section('content')
-    <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <h1>
-                    Create Campaign
 
-                </h1>
-                <ol class="breadcrumb">
-                    <li><a href="index.html"><i class="fa fa-dashboard"></i> Dashboard</a> </li>
-                    <li><a href="#"><i class="fa fa-th"></i> Create Campaign</a> </li>
+        <!-- Content Header (Page header) -->
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Create Campaign
 
-                </ol>
-            </section>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="index.html"><i class="fa fa-dashboard"></i> Dashboard</a> </li>
+                <li><a href="#"><i class="fa fa-th"></i> Create Campaign</a> </li>
 
-            <!-- Main content -->
+            </ol>
+        </section>
 
-            <section class="content">
-                <div class="row">
-                    <div class="col-md-3">
-                        <div class="box box-solid">
-                            <div class="box-header with-border">
-                                <h4 class="box-title">Campaign Info</h4>
-                            </div>
-                            <div class="box-body">
-                                <!-- the events -->
-                                <div id="external-events">
-                                    <div class="external-event bg-green">Break Fast</div>
-                                    <div class="external-event bg-yellow">Late Morning</div>
-                                    <div class="external-event bg-aqua">Afternoon</div>
-                                    <div class="external-event bg-light-blue">Primetime</div>
-                                    <div class="external-event bg-red">Overnight</div>
-                                    <div class="checkbox">
+        <!-- Main content -->
 
-                                    </div>
+        <section class="content">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="box box-solid">
+                        <div class="box-header with-border">
+                            <h4 class="box-title">Campaign Info</h4>
+                        </div>
+                        <div class="box-body">
+                            <!-- the events -->
+                            <div id="external-events">
+                                <div class="external-event bg-green">Break Fast</div>
+                                <div class="external-event bg-yellow">Late Morning</div>
+                                <div class="external-event bg-aqua">Afternoon</div>
+                                <div class="external-event bg-light-blue">Primetime</div>
+                                <div class="external-event bg-red">Overnight</div>
+                                <div class="checkbox">
+
                                 </div>
                             </div>
-                            <!-- /.box-body -->
                         </div>
-                        <!-- /. box -->
-
+                        <!-- /.box-body -->
                     </div>
-                    <!-- /.col -->
-                    <div class="col-md-7">
-                        <div class="box box-primary">
-                            <div class="box-body no-padding">
-                                <!-- THE CALENDAR -->
-                                <div id="calendar"></div>
-                            </div>
-                            <!-- /.box-body -->
-                        </div>
-                        <!-- /. box -->
-                    </div>
-                    <!-- /.col -->
-                    <div class="col-lg-2 col-md-2 hidden-sm hidden-xs "></div>
-
-
+                    <!-- /. box -->
 
                 </div>
-                <!-- /.row -->
+                <!-- /.col -->
+                <div class="col-md-7">
+                    <div class="box box-primary">
+                        <div class="box-body no-padding">
+                            <!-- THE CALENDAR -->
+                            <div id="calendar"></div>
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /. box -->
+                </div>
+                <!-- /.col -->
+                <div class="col-lg-2 col-md-2 hidden-sm hidden-xs "></div>
 
-                <div class="container">
 
-                    <p align="right">
-                        <a href="create-campaign-page2.html"><button class="btn campaign-button" style="margin-right:15%">Next <i class="fa fa-play" aria-hidden="true"></i></button></a>
 
-                    </p>
-            </section>
+            </div>
+            <!-- /.row -->
 
-            <!-- /.content -->
-        </div>
+            <div class="container">
+
+                <p align="right">
+                    <a href="create-campaign-page2.html"><button class="btn campaign-button" style="margin-right:15%">Next <i class="fa fa-play" aria-hidden="true"></i></button></a>
+
+                </p>
+        </section>
+
+        <!-- /.content -->
+
 @stop
--- resources/views/campaign/create2.blade.php
+++ resources/views/campaign/create2.blade.php
@@ -2,137 +2,135 @@
 
 @section('content')
 
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <h1>
-                Create Campaign
+    <!-- Content Header (Page header) -->
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Create Campaign
 
-            </h1>
-            <ol class="breadcrumb" style="font-size: 16px">
+        </h1>
+        <ol class="breadcrumb" style="font-size: 16px">
 
-                <li><a href="#"><i class="fa fa-th"></i> Create Campaign</a> </li>
-                <li><a href="index.html"><i class="fa fa-address-card"></i> New Media</a> </li>
+            <li><a href="#"><i class="fa fa-th"></i> Create Campaign</a> </li>
+            <li><a href="index.html"><i class="fa fa-address-card"></i> New Media</a> </li>
 
-            </ol>
-        </section>
+        </ol>
+    </section>
 
-        <!-- Main content -->
+    <!-- Main content -->
 
-        <section class="content">
-            <div class="row">
-                <div class="col-md-1 hidden-sm hidden-xs"></div>
-                <div class="col-md-9 " style="padding:2%">
-                    <form class="campform">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <input type="text" placeholder="Name">
-                            </div>
-
-                            <div class="col-lg-6 col-md-6 hidden-sm hidden-xs"></div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <input type="text" placeholder="Brand">
-                            </div>
-                            <div class="col-md-6">
-                                <input type="text" placeholder="Product">
-                            </div>
+    <section class="content">
+        <div class="row">
+            <div class="col-md-1 hidden-sm hidden-xs"></div>
+            <div class="col-md-9 " style="padding:2%">
+                <form class="campform">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <input type="text" placeholder="Name">
                         </div>
 
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="input-group date styledate">
-                                    <div class="input-group-addon">
-                                        <i class="fa fa-calendar"></i>
-                                    </div>
-                                    <input type="text" placeholder="Start Date" class="form-control pull-right" id="datepicker" ">
-                                </div>
+                        <div class="col-lg-6 col-md-6 hidden-sm hidden-xs"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <input type="text" placeholder="Brand">
+                        </div>
+                        <div class="col-md-6">
+                            <input type="text" placeholder="Product">
+                        </div>
+                    </div>
 
-                                <div class="input-group date styledate">
-                                    <div class="input-group-addon">
-                                        <i class="fa fa-calendar"></i>
-                                    </div>
-                                    <input type="text" placeholder="End Date" class="form-control pull-right" id="datepickerend">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="input-group date styledate">
+                                <div class="input-group-addon">
+                                    <i class="fa fa-calendar"></i>
                                 </div>
+                                <input type="text" placeholder="Start Date" class="form-control pull-right" id="datepicker" ">
+                            </div>
+
+                            <div class="input-group date styledate">
+                                <div class="input-group-addon">
+                                    <i class="fa fa-calendar"></i>
+                                </div>
+                                <input type="text" placeholder="End Date" class="form-control pull-right" id="datepickerend">
                             </div>
                         </div>
+                    </div>
 
-                        <div class="row" style="margin-top:10%">
-                            <div class="col-md-2">
-                                <div class="form-group">
-                                    <p>
-                                        <label>
-                                            <input type="checkbox" class="minimal-red">
-                                            BreakFast
-                                        </label>
-                                    </p>
-                                    <p>
-                                        <label>
-                                            <input type="checkbox" class="minimal-red">
-                                            Late Morning
-                                        </label>
-                                    </p>
-                                    <p>
-                                        <label>
-                                            <input type="checkbox" class="minimal-red">
-                                            Afternoon
-                                        </label>
-                                    </p>
+                    <div class="row" style="margin-top:10%">
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <p>
+                                    <label>
+                                        <input type="checkbox" class="minimal-red">
+                                        BreakFast
+                                    </label>
+                                </p>
+                                <p>
+                                    <label>
+                                        <input type="checkbox" class="minimal-red">
+                                        Late Morning
+                                    </label>
+                                </p>
+                                <p>
+                                    <label>
+                                        <input type="checkbox" class="minimal-red">
+                                        Afternoon
+                                    </label>
+                                </p>
 
-                                </div>
-                            </div>
-
-                            <div class="col-md-2">
-                                <div class="form-group">
-                                    <p>
-                                        <label>
-                                            <input type="checkbox" class="minimal-red">
-                                            Prime Time
-                                        </label>
-                                    </p>
-                                    <p>
-                                        <label>
-                                            <input type="checkbox" class="minimal-red">
-                                            Late Night
-                                        </label>
-                                    </p>
-                                    <p>
-                                        <label>
-                                            <input type="checkbox" class="minimal-red">
-                                            Overnight
-                                        </label>
-                                    </p>
-
-                                </div>
                             </div>
                         </div>
 
-                    </form>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <p>
+                                    <label>
+                                        <input type="checkbox" class="minimal-red">
+                                        Prime Time
+                                    </label>
+                                </p>
+                                <p>
+                                    <label>
+                                        <input type="checkbox" class="minimal-red">
+                                        Late Night
+                                    </label>
+                                </p>
+                                <p>
+                                    <label>
+                                        <input type="checkbox" class="minimal-red">
+                                        Overnight
+                                    </label>
+                                </p>
 
-                </div>
-                <!-- /.col -->
-                <div class="col-md-2 hidden-sm hidden-xs"></div>
-                <!-- /.col -->
+                            </div>
+                        </div>
+                    </div>
 
-
-
+                </form>
 
             </div>
-            <!-- /.row -->
+            <!-- /.col -->
+            <div class="col-md-2 hidden-sm hidden-xs"></div>
+            <!-- /.col -->
 
-            <div class="container">
 
-                <p align="right">
-                    <a href="create-campaign-page3.html"><button class="btn campaign-button" style="margin-right:15%">Next <i class="fa fa-play" aria-hidden="true"></i></button></a>
 
-                </p>
-        </section>
 
-        <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
+        </div>
+        <!-- /.row -->
+
+        <div class="container">
+
+            <p align="right">
+                <a href="create-campaign-page3.html"><button class="btn campaign-button" style="margin-right:15%">Next <i class="fa fa-play" aria-hidden="true"></i></button></a>
+
+            </p>
+    </section>
+
+    <!-- /.content -->
+
+<!-- /.content-wrapper -->
 
 @endsection
