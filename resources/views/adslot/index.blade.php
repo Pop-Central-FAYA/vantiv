dd3befda18c4f43c419b70755236f14641137e39
rheedwahn <rabusari@student.lautech.edu.ng>
2017-11-16 13:21:01 +0100

    first push to this branch
    Former-commit-id: 95407a26ac8ba29a3b580d79a56277cef92a1f61

--- resources/views/adslot/index.blade.php
+++ resources/views/adslot/index.blade.php
@@ -81,7 +81,7 @@
                                             </thead>
                                             <tbody>
                                             @foreach($adslot as $adslots)
-                                            <tr>
+                                            <tr id="{{ $adslots->id }}">
                                                 <td>Monday</td>
 
                                                 <td>{{ $adslots->hourly_range[0]->time_range }}</td>
@@ -89,9 +89,9 @@
                                                     <td>{{ ((object)($rating))->price }}</td>
                                                 @endforeach
                                                 <td>
-                                                    <a href="#" style="font-size: 16px"><span class="label label-warning" data-toggle="modal" data-target=".bs-example1-modal-md" style="cursor: pointer;">  Edit</span></a>
+                                                    <a href="#" id="edit" edit_adslot_id = "{{ $adslots->id }}" style="font-size: 16px"><span class="label label-warning" data-toggle="modal" data-target=".bs-example1-modal-md" style="cursor: pointer;">  Edit</span></a>
 
-                                                    <a href="#" style="font-size: 16px"><span class="label label-danger">  <i class="fa fa-trash"></i></span></a></td>
+                                                    {{--<a href="#" style="font-size: 16px"><span class="label label-danger">  <i class="fa fa-trash"></i></span></a></td>--}}
                                             </tr>
                                             @endforeach
                                             </tbody>
@@ -138,7 +138,7 @@
                             <form class="selsec" style="margin-left:">
                                 {{--@foreach($adslot as $adslots)--}}
                                     @foreach($adslots->rate_card as $rating)
-                                        <p align="center"><input type="radio" name="duration" value="sec"> <span style="margin-left: 10px;">{{ ((object)($rating))->time_in_seconds_id }}</span> <span class="sec-amount">{{ ((object)($rating))->price }}</span></p>
+                                        <p align="center"><input type="radio" name="duration" value="sec"> <span style="margin-left: 10px;"></span> <span class="sec-amount">{{ ((object)($rating))->price }}</span></p>
                                     @endforeach
 
                                 {{--@endforeach--}}
@@ -161,91 +161,100 @@
 @section('scripts')
     {!! HTML::script('assets/js/moment.min.js') !!}
     {!! HTML::script('assets/js/bootstrap-datetimepicker.min.js') !!}
-                    <script>
-                        $(function () {
-                            $("#example1").DataTable();
-                            $('#example2').DataTable({
-                                "paging": true,
-                                "lengthChange": false,
-                                "searching": false,
-                                "ordering": true,
-                                "info": true,
-                                "autoWidth": false
-                            });
-                        });
-                    </script>
 
-                    <script>
-                        $(function () {
-                            //Initialize Select2 Elements
-                            $(".select2").select2();
+    <script>
+        $(document).ready(function() {
+            $("body").delegate("#edit", "click", function() {
+               var id = $(this).attr("edit_adslot_id");
 
-                            //Datemask dd/mm/yyyy
-                            $("#datemask").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
-                            //Datemask2 mm/dd/yyyy
-                            $("#datemask2").inputmask("mm/dd/yyyy", {"placeholder": "mm/dd/yyyy"});
-                            //Money Euro
-                            $("[data-mask]").inputmask();
+            });
+        })
+    </script>
+    <script>
+        $(function () {
+            $("#example1").DataTable();
+            $('#example2').DataTable({
+                "paging": true,
+                "lengthChange": false,
+                "searching": false,
+                "ordering": true,
+                "info": true,
+                "autoWidth": false
+            });
+        });
+    </script>
 
-                            //Date range picker
-                            $('#reservation').daterangepicker();
-                            //Date range picker with time picker
-                            $('#reservationtime').daterangepicker({timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A'});
-                            //Date range as a button
-                            $('#daterange-btn').daterangepicker(
-                                {
-                                    ranges: {
-                                        'Today': [moment(), moment()],
-                                        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                                        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                                        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                                        'This Month': [moment().startOf('month'), moment().endOf('month')],
-                                        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-                                    },
-                                    startDate: moment().subtract(29, 'days'),
-                                    endDate: moment()
-                                },
-                                function (start, end) {
-                                    $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-                                }
-                            );
+    <script>
+        $(function () {
+            //Initialize Select2 Elements
+            $(".select2").select2();
 
-                            //Date picker
-                            $('#datepicker').datepicker({
-                                autoclose: true
-                            });
+            //Datemask dd/mm/yyyy
+            $("#datemask").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
+            //Datemask2 mm/dd/yyyy
+            $("#datemask2").inputmask("mm/dd/yyyy", {"placeholder": "mm/dd/yyyy"});
+            //Money Euro
+            $("[data-mask]").inputmask();
 
-                            $('#datepickerend').datepicker({
-                                autoclose: true
-                            });
+            //Date range picker
+            $('#reservation').daterangepicker();
+            //Date range picker with time picker
+            $('#reservationtime').daterangepicker({timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A'});
+            //Date range as a button
+            $('#daterange-btn').daterangepicker(
+                {
+                    ranges: {
+                        'Today': [moment(), moment()],
+                        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                        'This Month': [moment().startOf('month'), moment().endOf('month')],
+                        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                    },
+                    startDate: moment().subtract(29, 'days'),
+                    endDate: moment()
+                },
+                function (start, end) {
+                    $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+                }
+            );
 
-                            //iCheck for checkbox and radio inputs
-                            $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
-                                checkboxClass: 'icheckbox_minimal-blue',
-                                radioClass: 'iradio_minimal-blue'
-                            });
-                            //Red color scheme for iCheck
-                            $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
-                                checkboxClass: 'icheckbox_minimal-red',
-                                radioClass: 'iradio_minimal-red'
-                            });
-                            //Flat red color scheme for iCheck
-                            $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
-                                checkboxClass: 'icheckbox_flat-green',
-                                radioClass: 'iradio_flat-green'
-                            });
+            //Date picker
+            $('#datepicker').datepicker({
+                autoclose: true
+            });
 
-                            //Colorpicker
-                            $(".my-colorpicker1").colorpicker();
-                            //color picker with addon
-                            $(".my-colorpicker2").colorpicker();
+            $('#datepickerend').datepicker({
+                autoclose: true
+            });
 
-                            //Timepicker
-                            $(".timepicker").timepicker({
-                                showInputs: false
-                            });
-                        });
-                    </script>
+            //iCheck for checkbox and radio inputs
+            $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+                checkboxClass: 'icheckbox_minimal-blue',
+                radioClass: 'iradio_minimal-blue'
+            });
+            //Red color scheme for iCheck
+            $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+                checkboxClass: 'icheckbox_minimal-red',
+                radioClass: 'iradio_minimal-red'
+            });
+            //Flat red color scheme for iCheck
+            $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+                checkboxClass: 'icheckbox_flat-green',
+                radioClass: 'iradio_flat-green'
+            });
+
+            //Colorpicker
+            $(".my-colorpicker1").colorpicker();
+            //color picker with addon
+            $(".my-colorpicker2").colorpicker();
+
+            //Timepicker
+            $(".timepicker").timepicker({
+                showInputs: false
+            });
+        });
+    </script>
 @stop
 
 @section('style')
