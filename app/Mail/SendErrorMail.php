67b4639a2863a734805497b210b05a9b06518a60
rheedwahn <rabusari@student.lautech.edu.ng>
2018-04-23 14:18:54 +0100

    Fixes
    Former-commit-id: 765f55fa1cb7803cd52a7d9f57c76d983de41c75

---
+++ app/Mail/SendErrorMail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Vanguard\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendErrorMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+
+    public $content;
+
+    public function __construct($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mail.error_mail')->with('content', $this->content);
+    }
+}
