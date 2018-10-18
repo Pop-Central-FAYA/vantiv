33e06c4a434af039965a2bb80ee21e962d9b7b81
Joshua Adeyemi <joshua@fayamedia.com>
2018-10-18 11:54:13 +0100

    remove links to pages that do not exist
    Former-commit-id: 85bf4ad3980cdcccc5f6e1b84c66637e9e69b1a7

--- resources/views/partials/new-frontend/broadcaster/campaign_management/sidebar.blade.php
+++ resources/views/partials/new-frontend/broadcaster/campaign_management/sidebar.blade.php
@@ -8,11 +8,11 @@
     <div class="_nav">
         <ul>
             <li class="campaign_icon {{ Request::is('campaign-management/dashboard') ? 'active' : ''  }}"><a href="{{ route('broadcaster.campaign_management') }}">Dashboard</a></li>
-            <li class="report_icon"><a href="">Reports</a></li>
+            <!-- <li class="report_icon"><a href="">Reports</a></li> -->
 
             <li class="clients_icon {{ Request::is('walk-in') ? 'active' : '' || Request::is('walk-in/walk-in/details/*') ? 'active' : ''  }}"><a href="{{ route('walkins.all') }}">Walk-Ins</a></li>
             <li class="invoice_icon {{ Request::is('mpos/*') ? 'active' : ''  }}"><a href="{{ route('all-mpos') }}">MPO</a></li>
-            <li class="settings_icon {{ Request::is('agency/user/manage') ? 'active' : '' }}"><a href="">User Management</a></li>
+            <!-- <li class="settings_icon {{ Request::is('agency/user/manage') ? 'active' : '' }}"><a href="">User Management</a></li> -->
         </ul>
     </div>
 
