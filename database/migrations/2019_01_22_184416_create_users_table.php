d805230d1eefe6ad1bef93072b4ca62038f6d684
rheedwahn <ridwan2bus@gmail.com>
2019-01-22 20:35:35 +0100

    include the query to migrate the users from the old users table to the new users table
    Former-commit-id: 0b09efdd167929ae52031b17c7248a7666f0fc40

--- database/migrations/2019_01_22_184416_create_users_table.php
+++ database/migrations/2019_01_22_184416_create_users_table.php
@@ -34,6 +34,15 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        DB::statement("INSERT into users (id, email, username, password, firstname, lastname, phone_number, avatar, 
+                                                address, country_id, birthday, last_login, confirmation_token, status, 
+                                                two_factor_country_code, two_factor_phone, two_factor_options, 
+                                                remember_token, created_at, updated_at)
+                                    SELECT id, email, null, password, firstname, lastname, phone_number, null, '', 
+                                    null, null, null, null, 'Active', null, null, null, null, time_created, 
+                                    time_modified from user_old
+                        ");
     }
     /**
      * Reverse the migrations.
--- database/sql/migrate_user_old_to_users.sql
+++
@@ -1,3 +0,0 @@
-
-INSERT into users (id, email, username, password, firstname, lastname, phone_number, avatar, address, country_id, birthday, last_login, confirmation_token, status, two_factor_country_code, two_factor_phone, two_factor_options, remember_token, created_at, updated_at)
-SELECT id, email, null, password, firstname, lastname, phone_number, null, '', null, null, null, null, 'Active', null, null, null, null, time_created, time_modified from user_old
