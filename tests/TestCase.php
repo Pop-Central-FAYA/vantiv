9107e9ee1bd99d59e8d36a1496029e8e28142652
rheedwahn <ridwan2bus@gmail.com>
2019-02-02 22:27:27 +0100

    In this commit, the following were done: 1.Remove the test folder that came with the boiler template to ensure everything is fresh 2.set up a fata_test db for the application so that the test can run migration and rollback 3.test out a simple testcase to see if all is fine with the enviroment
    Former-commit-id: 0c5a74f451846c2665fbc4e86a6e47993f16b058

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,53 +2,10 @@
 
 namespace Tests;
 
-class TestCase extends \Laravel\BrowserKitTesting\TestCase
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
 {
-    protected $seed = true;
-
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://vanguard.dev';
-
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
-    public function createApplication()
-    {
-        ini_set('memory_limit', '2048M');
-
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-        return $app;
-    }
-
-    protected function refreshAppAndExecuteCallbacks()
-    {
-        $oldSeed = $this->seed;
-        $this->seed = $this->isSQLiteConnection();
-
-        $this->refreshApplication();
-        $this->executeCallbacks();
-
-        $this->seed = $oldSeed;
-    }
-
-    protected function executeCallbacks()
-    {
-        foreach ($this->afterApplicationCreatedCallbacks as $callback) {
-            call_user_func($callback);
-        }
-    }
-
-    protected function isSQLiteConnection()
-    {
-        return \DB::connection() instanceof \Illuminate\Database\SQLiteConnection;
-    }
+    use CreatesApplication, RefreshDatabase;
 }
