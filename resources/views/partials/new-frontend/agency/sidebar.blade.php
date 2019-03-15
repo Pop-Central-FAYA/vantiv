f852467adf98f4b0765d079da1211db48ec54232
rheedwahn <ridwan2bus@gmail.com>
2019-03-15 15:04:38 +0100

    created a role for admin to see all part of the application
    Former-commit-id: ddd0f51125a347748c15108e4f54fd314d8671bf

--- resources/views/partials/new-frontend/agency/sidebar.blade.php
+++ resources/views/partials/new-frontend/agency/sidebar.blade.php
@@ -3,24 +3,27 @@
         <img src="{{ asset('new_frontend/img/logo.svg') }}">
     </div>
 
-
     <div class="_nav">
         <ul>
+            @if(Auth::user()->hasRole('admin') || Auth::user()->hasRole('finance') || Auth::user()->hasRole('compliance'))
             <li class="campaign_icon {{ Request::is('/') ? 'active' : ''  }}"><a href="{{ route('dashboard') }}">Dashboard</a></li>
+            @endif
             <!-- <li class="report_icon"><a href="">Reports</a></li> -->
-            @if(!Auth::user()->hasRole('compliance'))
+            @if(Auth::user()->hasRole('admin'))
                 <li class="clients_icon {{ Request::is('agency/clients/*') ? 'active' : ''  }}"><a href="{{ route('clients.list') }}">Clients</a></li>
             @endif
-            @if(Auth::user()->hasRole('finance'))
+            @if(Auth::user()->hasRole('finance') || Auth::user()->hasRole('admin') || Auth::user()->hasRole('compliance'))
                 <li class="invoice_icon {{ Request::is('agency/invoices/*') ? 'active' : ''  }}"><a href="{{ route('invoices.all') }}">Invoices</a></li>
+            @endif
+            @if(Auth::user()->hasRole('finance') || Auth::user()->hasRole('admin'))
                 <li class="wallet_icon {{ Request::is('agency/wallets/*') ? 'active' :  Request::is('agency/wallets/wallet-statement') ? 'active' : ''  }}"><a href="{{ route('agency_wallet.statement') }}">Wallet</a></li>
-        @endif
+            @endif
         <!-- <li class="settings_icon {{ Request::is('agency/user/manage') ? 'active' : '' }}"><a href="{{ route('agency.user_management') }}">User Management</a></li> -->
         </ul>
     </div>
-    @if(!Auth::user()->hasRole('compliance'))
+    @if(Auth::user()->hasRole('admin'))
         <div class="_nav_button">
             <a href="{{ route('campaign.get_campaign_general_information') }}" class="btn full block_disp uppercased align_center"><span class="_plus"></span>New Campaign</a>
         </div>
-        @endif
+    @endif
 </div>
