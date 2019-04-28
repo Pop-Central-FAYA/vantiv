2e06a6834e1a2fc077675e1c05e567fb61f48d5a
cunlayogd <ogundeleadekunle@gmail.com>
2019-04-28 09:44:51 +0100

    style report graph
    Former-commit-id: db470398d4d72ede8f76e6f22f49118b5a7e949e

--- resources/views/broadcaster_module/dashboard/campaign_management/dashboard.blade.php
+++ resources/views/broadcaster_module/dashboard/campaign_management/dashboard.blade.php
@@ -52,7 +52,7 @@
                             </div>
                             <div class="col-8 px-0">
                                 <span class="text-muted">Pepsi is your biggest spender</span>
-                                <h3 class="text-success text-center mt-2"><b>2,050,000</b></h3>
+                                <h3 class="text-success text-center mt-2"><b>7,800,000</b></h3>
                             </div>
                         </div>
                     </div>
@@ -69,7 +69,7 @@
                         <h3 class="card-title my-3 text-muted">132 Campaigns</h3>
                         <div class="row dashboard_pies">
                             <div class="col-7">
-                                <div id="tv" class="_pie_chart" style="height: 100px"></div>
+                                <div id="tv" class="_pie_chart" style="height: 130px"></div>
                             </div>
                             <div class="col-5 pt-5">
                                 <ul>
@@ -109,7 +109,7 @@
                         <h3 class="card-title my-3 text-muted">74 Campaigns</h3>
                         <div class="row dashboard_pies">
                             <div class="col-7">
-                                <div id="tv" class="_pie_chart" style="height: 100px"></div>
+                                <div id="radio" class="_pie_chart" style="height: 130px"></div>
                             </div>
                             <div class="col-5 pt-5">
                                 <ul>
@@ -139,42 +139,50 @@
                 </div>
             </div>
         </div>
-    </div>
-
-    <!-- client charts -->
-        @if(Auth::user()->companies()->count() > 1)
-            <div class="clearfix periodic_rev">
-                <h3><p>Periodic Revenue Chart</p></h3>
-                <br>
-                <div class="row">
-                    <div class="column col_3 clearfix">
-                        <select name="filter_year" class="filter_year" id="filter_year">
-                            @foreach($year_list as $year)
-                                <option
-                                    @if($current_year == $year)
-                                        selected
-                                    @endif
-                                    value="{{ $year }}">
-                                    {{ $year }}
-                                </option>
-                            @endforeach
-                        </select>
+        <div class="row my-5">
+            <div class="col-12 pb-1 mb-3" style="border-bottom: 2px solid #44c1c9;">
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <button id="tv-toggle" type="button" class="btn btn-info mr-2"><i class="material-icons">tv</i> TV</button>
+                    <button id="radio-toggle" type="button" class="btn btn-info"><i class="material-icons">radio</i> RADIO</button>
+                </div>
+            </div>
+            <!-- client charts -->
+            @if(Auth::user()->companies()->count() > 1)
+                <div class="col-12">
+                    <div class="row mb-2">
+                        <div class="col-6"></div>
+                        <div class="col-6 text-right">
+                            <select name="filter_year" class="filter_year" id="filter_year">
+                                @foreach($year_list as $year)
+                                    <option
+                                        @if($current_year == $year)
+                                            selected
+                                        @endif
+                                        value="{{ $year }}">
+                                        {{ $year }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
                 </div>
                 <br>
-                {{--Periodic revenue chart goes here--}}
-                <div class="row">
+                <!-- {{--Periodic revenue chart goes here--}} -->
+                <div class="col-12">
                     <div id="periodicChart" style="min-width: 310px; height: 400px; margin: 0 auto">
                 </div>
-            </div>
-        @else
-            <div class="clearfix">
-                <h4><p>Total Volume of campaign</p></h4>
-                <br>
-                {{--Total Volume of campaigns graph goes here--}}
-                <div id="containerTotal" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
-            </div>
-        @endif
+            @else
+                <div class="clearfix">
+                    <h4><p>Total Volume of campaign</p></h4>
+                    <br>
+                    {{--Total Volume of campaigns graph goes here--}}
+                    <div id="containerTotal" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+                </div>
+            @endif
+        </div>
+    </div>
+
+    
     </div>
 
 
@@ -218,6 +226,67 @@
             flatpickr(".flatpickr", {
                 altInput: true,
             });
+
+            // $(".dashboard_campaigns_filtered").dataTable().fnDestroy();
+
+            // var campaignFilter =  $('.dashboard_campaigns').DataTable({
+            //     dom: 'Blfrtip',
+            //     paging: true,
+            //     serverSide: true,
+            //     processing: true,
+            //     aaSorting: [],
+            //     oLanguage: {
+            //         sLengthMenu: "_MENU_"
+            //     },
+            //     ajax: {
+            //         url: '/agency/dashboard/campaigns',
+            //         data: function (d) {
+            //             d.start_date = $('input[name=start_date]').val();
+            //             d.stop_date = $('input[name=stop_date]').val();
+            //             d.filter_user = $('#filter_user').val();
+            //         }
+            //     },
+            //     columns: getColumns(),
+
+            // });
+
+            function getColumns()
+            {
+                if(companies > 1){
+                    return [
+                                    // {data: 'id', name: 'id'},
+                                    {data: 'name', name: 'name'},
+                                    {data: 'brand', name: 'brand'},
+                                    {data: 'start_date', name: 'start_date'},
+                                    {data: 'budget', name: 'budget'},
+                                    {data: 'adslots', name: 'adslots'},
+                                    {data: 'status', name: 'status'},
+                                    {data: 'station', name: 'station'}
+                                ]
+                }else{
+                    return [
+                                    // {data: 'id', name: 'id'},
+                                    {data: 'name', name: 'name'},
+                                    {data: 'brand', name: 'brand'},
+                                    {data: 'start_date', name: 'start_date'},
+                                    {data: 'budget', name: 'budget'},
+                                    {data: 'adslots', name: 'adslots'},
+                                    {data: 'status', name: 'status'},
+                                ]
+                }
+            }
+
+            // $('#dashboard_filter_campaign').on('click', function() {
+            //     campaignFilter.draw();
+            // });
+
+            // $('.key_search').on('keyup', function(){
+            //     campaignFilter.search($(this).val()).draw() ;
+            // })
+
+            // $('#filter_user').on('change', function() {
+            //     campaignFilter.draw();
+            // });
 
             function channel_pie(channel, percent_active, percent_pending, percent_finished){
                 Highcharts.chart(channel,{
