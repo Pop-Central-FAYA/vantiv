69708fe8ec929ddad9af4bdd4910dac6f20ceae7
dammyson <aayobami@fayamedia.com>
2019-07-03 22:03:40 +0100

    Added Vantage Branding to all the pages

--- resources/views/dsp_layouts/auth.blade.php
+++ resources/views/dsp_layouts/auth.blade.php
@@ -10,7 +10,7 @@
     <meta property="og:title" content="FAYA" />
     <meta property="og:description" content="Advertising" />
 
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('new_frontend/img/favicon.ico') }}" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('new_frontend/img/fav_icon.ico') }}" />
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
 
     <link href="{{ asset('new_frontend/css/reset.css') }}" rel="stylesheet">
@@ -29,7 +29,7 @@
 
 
 <div class="login_logo">
-    <img src="{{ asset('new_frontend/img/logo.svg') }}">
+<svg width="119px" height="36px" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 553.34 216.48"><defs><style>.cls-1{fill:#64c4ce;}.cls-2{fill:#575758;}.cls-3{fill:#4eaeaf;}.cls-4{font-size:91.26px;fill:#1d1d1d;font-family:LemonMilk, "Lemon/Milk";}.cls-5{letter-spacing:-0.12em;}.cls-6{letter-spacing:-0.07em;}</style></defs><title>Vantage+Torch_Logos</title><polygon class="cls-1" points="109.8 126.06 143.69 33.21 182.71 33.21 123.02 154.83 109.8 126.06"/><polygon class="cls-1" points="153.81 33.21 182.71 33.21 109.17 183.1 101.38 140.09 153.81 33.21"/><polygon class="cls-2" points="76.86 33.21 122.76 127.08 168.66 33.21 76.86 33.21"/><polygon class="cls-3" points="123.6 153.64 109.13 183.06 35.66 33.14 64.58 33.21 123.6 153.64"/><text class="cls-4" transform="translate(151.36 154.83)">AN<tspan class="cls-5" x="135.77" y="0">T</tspan><tspan class="cls-6" x="178.02" y="0">A</tspan><tspan x="240.44" y="0">GE</tspan></text></svg>
 </div>
 
 
