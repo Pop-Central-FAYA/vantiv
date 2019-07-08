1a03e42aa03fe735e993e71ba04f6b765e48a8b6
Adekunle Ogundele <ogundeleadekunle@gmail.com>
2019-07-08 09:02:27 +0000

    onupload display assets without reloading the page, UI changes

--- resources/assets/js/components/asset_management/DeleteAsset.vue
+++ resources/assets/js/components/asset_management/DeleteAsset.vue
@@ -1,19 +1,17 @@
 <template>
   <v-layout>
-    <v-dialog v-model="dialog" persistent max-width="300px">
+    <v-dialog v-model="dialog" persistent max-width="600px">
         <template v-slot:activator="{ on }">
-            <!-- <v-btn color="red" small dark v-on="on"> -->
-                <v-icon color="red" dark v-on="on" right>delete</v-icon>
-            <!-- </v-btn> -->
+            <v-icon color="red" dark v-on="on" right>delete</v-icon>
         </template>
       <v-card>
-        <v-card-text>
-            Are you sure you want to delete this media asset?
+        <v-card-text style="padding: 40px 20px;">
+            Are you sure you want to delete media asset with file name "{{asset.file_name}}" and a duration of {{asset.duration}} seconds?
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="success" dark @click="dialog = false">No</v-btn>
-          <v-btn color="success" dark @click="deleteAsset()">Yes</v-btn>
+          <v-btn class="default-vue-btn" dark @click="dialog = false">No</v-btn>
+          <v-btn color="red" dark @click="deleteAsset()">Yes</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -23,7 +21,7 @@
 <script>
   export default {
     props : {
-      assetId: String
+      asset: Object
     },
     data() {
       return {
@@ -40,7 +38,7 @@
         this.sweet_alert(msg, 'info');
         axios({
             method: 'get',
-            url: '/media-assets/delete/'+this.assetId
+            url: '/media-assets/delete/'+this.asset.id
         }).then((res) => {
             console.log(res.data);
             if (res.data.status == 'success') {
