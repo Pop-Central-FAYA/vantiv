0c6337a9623ae7989219455998e449edcfac897e
dammyson <aayobami@fayamedia.com>
2019-07-17 09:01:36 +0100

    fixe corrections

--- webpack.mix.js
+++ webpack.mix.js
@@ -27,6 +27,9 @@ mix.options({
 // mix.js('resources/js/app.js', 'public/js')
 //     .sass('resources/sass/app.scss', 'public/css');
 mix.js('resources/assets/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css')
-    .version();
+    .sass('resources/assets/sass/app.scss', 'public/css');
+
+  if (mix.inProduction()) {
+    mix.version();
+    }
 mix.autoExtract();
