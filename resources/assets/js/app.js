3f06f42d154c6c02b7c451114d1f8c56747824bd
Adekunle Ogundele <ogundeleadekunle@gmail.com>
2019-07-29 18:10:02 +0000

    Resolve VAN-168 "Customise plan"

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -39,6 +39,11 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 import Multiselect from 'vue-multiselect';
 Vue.component('multiselect', Multiselect);
 
+// Vue time picker
+import VueTimepicker from 'vue2-timepicker';
+Vue.component('vue-timepicker', VueTimepicker);
+
+
 // declared to manage events globally
 window.Event = new Vue();
 /**
@@ -56,6 +61,8 @@ Vue.component('media-plan-suggestion-filter', require('./components/media_plan/c
 Vue.component('media-plan-footer-nav', require('./components/media_plan/customise/FooterNavigation.vue'));
 Vue.component('media-plan-suggestions', require('./components/media_plan/customise/Suggestions.vue'));
 Vue.component('media-plan-create-campaign', require('./components/media_plan/summary/CreateCampaign.vue'));
+Vue.component('media-plan-details', require('./components/media_plan/complete/PlanDetails.vue'));
+Vue.component('media-plan-program-details', require('./components/media_plan/complete/ProgramDetails.vue'));
 Vue.component('media-plan-summary', require('./components/media_plan/summary/Summary.vue'));
 Vue.component('media-plan-criteria-form', require('./components/media_plan/CriteriaForm.vue'));
 Vue.component('media-plan-list', require('./components/media_plan/AllMediaPlans.vue'));
@@ -128,6 +135,12 @@ Vue.mixin({
         formatDate(date_str) {
             var dateParts = date_str.split("-");
             return `${dateParts[0]}-${dateParts[1]}-${dateParts[2].substr(0,2)}`;
+        },
+        shortDay(str) {
+            return str.substring(0, 3);
+        },
+        formatAmount(number) {
+            return number.toLocaleString();
         },
         dateToHumanReadable(date) {
             return moment(date).format('Do-MMM-YYYY');
