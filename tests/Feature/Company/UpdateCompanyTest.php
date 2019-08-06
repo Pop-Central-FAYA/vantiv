22ebe0616c462d6aee75dd4d6dcf9912da3ab1b8
dammyson <aayobami@fayamedia.com>
2019-08-06 16:17:52 +0100

    new update to the pr

--- tests/Feature/Company/UpdateCompanyTest.php
+++ tests/Feature/Company/UpdateCompanyTest.php
@@ -26,27 +26,7 @@ class UpdateCompanyTest extends TestCase
         return $this->actingAs($user)->postJson(route($this->route_name), $data);
     }
 
-    public function test_unauthenticated_user_cannot_access_company_update_route()
-    {
-        \Session::start();
-        $response = $this->postJson(route('company.update'), [
-            '_token' => csrf_token()
-        ]);
-        $response->assertStatus(401);
-
-    }
-    public function test_user_without_update_permissions_cannot_company_update_access_route()
-    {
-        \Session::start();
-        $data = [
-            '_token' => csrf_token()
-        ];
-        $user = $this->setupAuthUser();
-        $response = $this->getResponse($user, $data);
-        $response->assertStatus(403);
-    }
-
-    public function test_authentication_user_with_permissions_cann_update_company_with_route()
+    public function test_authentication_user_can_update_company_with_route()
     {
         \Session::start();
         $data = [
@@ -58,6 +38,20 @@ class UpdateCompanyTest extends TestCase
         $user = $this->setupAuthUser();
         $response = $this->getResponse($user, $data);
         $response->assertStatus(200);
+    }
+
+    public function test_invalid_data_is_validated_on_update()
+    {
+        \Session::start();
+        $data = [
+            '_token' => csrf_token(),
+            'address' => '',
+            'image_url' => ""
+
+        ];
+        $user = $this->setupAuthUser();
+        $response = $this->getResponse($user, $data);
+        $response->assertStatus(422);
     }
 
 }
