9012b5601adf9ae4263acac702bfa7e09609783b
Adekunle Ogundele <ogundeleadekunle@gmail.com>
2019-09-06 01:42:03 +0000

    Resolve VAN-626 "Duplicate header"

--- resources/views/agency/user-management/invite_user.blade.php
+++ resources/views/agency/user-management/invite_user.blade.php
@@ -6,16 +6,8 @@
 
 @section('content')
     <div class="main_contain">
-        <!-- header -->
-    @if(Session::get('broadcaster_id'))
-        @include('partials.new-frontend.broadcaster.header')
-        @include('partials.new-frontend.broadcaster.campaign_management.sidebar')
-    @else
-        @include('partials.new-frontend.agency.header')
-    @endif
-
     <!-- subheader -->
-        <div class="sub_header clearfix mb pt">
+        <div class="sub_header clearfix mb">
             <div class="column col_6">
                 <h2 class="sub_header">Invite User(s)</h2>
             </div>
