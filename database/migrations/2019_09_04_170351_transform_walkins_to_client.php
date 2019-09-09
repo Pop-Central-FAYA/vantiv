d63e776a55fcb4b395dc98a1e67870ee64dac6d0
dammyson <aayobami@fayamedia.com>
2019-09-09 14:01:27 +0100

    Fixed collum name

--- database/migrations/2019_09_04_170351_transform_walkins_to_client.php
+++ database/migrations/2019_09_04_170351_transform_walkins_to_client.php
@@ -15,7 +15,7 @@ class TransformWalkinsToClient extends Migration
     {
         DB::statement(
             "INSERT INTO clients (id, nationality, name, image_url, created_by, company_id, street_address, created_at, updated_at)
-            SELECT id, nationality, company_name, image_url, user_id, company_id, location, time_modified, time_created FROM walkins where agency_id <>''" 
+            SELECT id, nationality, company_name, image_url, user_id, company_id, location, time_modified, time_created FROM walkIns where agency_id <>''" 
         );
     }
 
