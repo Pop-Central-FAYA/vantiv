980f13aecaca544b3c7893362fd0cf0f28cc3509
Joshua <joshua@fayamedia.com>
2019-09-14 09:14:16 +0000

    Resolve VAN-574 "Stations listing"

--- app/Libraries/MpsImporter/TvUserActivities.php
+++ app/Libraries/MpsImporter/TvUserActivities.php
@@ -15,19 +15,18 @@ use Vanguard\Libraries\Batch\LaravelBatch;
 
 class TvUserActivities
 {
-    // const CHUNK_BATCH = 5000;
     const CHUNK_BATCH = 20000;
 
     public function __construct($import_time, $csv_file)
     {
-        $this->import_time = $import_time;
+        $this->formatted_time = $import_time->format('Y-m-d H:i:s');
         $this->csv_file = $csv_file;
         $this->wave = null;
     }
 
     public function process()
     {   
-        $this->tv_station_list = TvStation::all();
+        $this->tv_station_map = $this->getTvStationMap();
         return DB::transaction(function(){
             $file_handle = fopen($this->csv_file, "r");
             $header = fgetcsv($file_handle);
@@ -37,71 +36,84 @@ class TvUserActivities
         });
     }
 
+    protected function getTvStationMap()
+    {
+        $tv_stations = TvStation::all();
+        $station_and_state = $tv_stations->groupBy(function($item) {
+            return "{$item->name}{$item->state}{$item->city}";
+        });
+        $station_only = $tv_stations->groupBy(function($item) {
+            return $item->name;
+        });
+        return array_merge($station_only->toArray(), $station_and_state->toArray());
+    }
+
     protected function storeActivities($file_handle, $header) {
         $activity_list = [];
-
-        $formatted_time = $this->import_time->format('Y-m-d H:i:s');
         $current_count = 0;
 
         Log::info("................................BEGIN PARSING OF TV DATA................................");
 
         while (($row_data = fgetcsv($file_handle)) !== false) {
             if (count($row_data) == 1) {
-                Log::info("This row only has a count of 1, so skipping");
+                Log::debug("This row only has a count of 1, so skipping");
                 continue;
             }
 
-            $current_count++;
-            $activity = [];
             $wave = $this->getWave($header, $row_data);
             $ext_profile_id = $row_data[0]; //the assumption is the ext_profile id is in the first column
 
             foreach ($row_data as $index => $value) {
                 if ($value == 1) {
                     $key = $header[$index];
-                    $station = TvStationParser::parseRgx($key);
-                    if ($station->isNotEmpty()) {
-                        $activity = $this->formatActivity($station);
-                        if (is_array($activity)) {
-                            $current_count++;
-
-                            $activity['id'] = uniqid();
-                            $activity['ext_profile_id'] = $ext_profile_id;
-                            $activity['wave'] = $wave;
-                            $activity['media_type'] = 'Tv';
-                            $activity['created_at'] = $formatted_time;
-                            $activity['updated_at'] = $formatted_time;
-
+                    $parsed_info = TvStationParser::parseRgx($key);
+                    if ($parsed_info->isNotEmpty()) {
+                        $activity = $this->processColumn($parsed_info, $ext_profile_id, $wave);
+                        if ($activity) {
                             $activity_list[] = $activity;
-
-                            echo($current_count);
-                            echo('.');
-
-                            Log::debug("{$key} = {$value}");
-                            Log::debug("{$station['name']} {$station['day']} {$station['timebelt']}");
+                            $current_count++;
+                            if (count($activity_list) >= static::CHUNK_BATCH) {
+                                $this->batchInsert($activity_list);
+                                $activity_list = [];
+                            }
+                            echo("{$current_count} ");
                         } else {
-                            Log::warning($key);
-                            Log::warning($station);
-                        }
-                    }
-                }
-
-                if (count($activity_list) >= static::CHUNK_BATCH) {
-                    $this->insertActivities($activity_list);
-                    $activity_list = [];
+                            Log::warning("Could not parse: {$key}");
+                        } 
+                    } 
                 }
             }
         }
-
-        $this->insertActivities($activity_list);
+        $this->batchInsert($activity_list);
         $activity_list = [];
 
-        echo(PHP_EOL);
         Log::info("................................END PARSING OF TV DATA................................");
         return $current_count;
     }
 
-    protected function insertActivities(&$activity_list) {
+    protected function processColumn($parsed_info, $ext_profile_id, $wave)
+    {
+        $tv_station = $this->getTvStation($parsed_info);
+        if ($tv_station) {
+            $formatted_timebelt = $this->formatTimeBelt($parsed_info['timebelt']);
+            return [
+                "id" => uniqid(),
+                'ext_profile_id' => $ext_profile_id,
+                "tv_station_id" => $tv_station['id'],
+                "tv_station_key" => $tv_station['key'],
+                "day" => $parsed_info['day'],
+                "start_time" => $formatted_timebelt[0],
+                "end_time" => $formatted_timebelt[1],
+                'wave' => $wave,
+                'media_type' => 'Tv',
+                'created_at' => $this->formatted_time,
+                'updated_at' => $this->formatted_time
+            ];
+        } 
+        return null;
+    }
+
+    protected function batchInsert(&$activity_list) {
         if (count($activity_list) > 0) {
             $mps_activity = new MpsProfileActivity();
             $columns = array_keys($activity_list[0]);
@@ -110,31 +122,19 @@ class TvUserActivities
         }
     }
 
-    protected function formatActivity($station)
+    protected function getTvStation($station)
     {
-        $available_stations = $this->tv_station_list
-            ->where('name', $station['name'])
-            ->where('state', $station['state'])
-            ->whereIn('city', ['', $station['city']])
-            ->where('type', $station['station_type']);
-
-        if ($available_stations->count() == 0) {
-            $available_stations = $this->tv_station_list->where('name', $station['name']);
+        $key = "{$station['name']}{$station['state']}{$station['city']}";
+        $tv_station = Arr::get($this->tv_station_map, $key, null);
+        if ($tv_station) {
+            return $tv_station[0];
+        }
+        //try with just the name as a fallback
+        $tv_station = Arr::get($this->tv_station_map, $station['name'], null);
+        if ($tv_station) {
+            return $tv_station[0];
         }
 
-        if ($available_stations->count() == 1) {
-            //do the real work here
-            $tv_station = $available_stations->first();
-            $formatted_timebelt = $this->formatTimeBelt($station['timebelt']);
-            return [
-                "tv_station_id" => $tv_station->id,
-                "tv_station_key" => $tv_station->key,
-                "day" => $station['day'],
-                "start_time" => $formatted_timebelt[0],
-                "end_time" => $formatted_timebelt[1]
-            ];
-        }
-        
         return null;
     }
 
