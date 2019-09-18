d591f05b52b1594255119b8e23f19c7b6bebf78e
dammyson <aayobami@fayamedia.com>
2019-09-18 11:52:36 +0100

    First commit for User managerment

--- app/Http/Controllers/Dsp/UserController.php
+++ app/Http/Controllers/Dsp/UserController.php
@@ -97,6 +97,7 @@ class UserController extends Controller
             $email_invitation_service = new UserInvitationMail($user_mail_content_array);
             $email_invitation_service->sendInvitationMail();
         });
+        
         return ['status'=>"success", 'message'=> "User(s) invited successfully, and emails sent"];
     }
 
