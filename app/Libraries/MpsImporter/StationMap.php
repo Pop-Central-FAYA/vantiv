9802d5a73047a3e3e5000e16cea80ad359e55dfc
Joshua Adeyemi <joshua@fayamedia.com>
2019-10-02 13:43:17 +0100

    refactoring how station ratings are parsed

---
+++ app/Libraries/MpsImporter/StationMap.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Vanguard\Libraries\MpsImporter;
+
+use Mpdf\Tag\Dd;
+use Symfony\Component\Yaml\Yaml;
+
+class StationMap
+{
+        
+    public function __construct()
+    {
+        $national_map = $this->parseYaml('tv-national.yml');
+        $network_map = $this->parseYaml('tv-national-network.yml');
+        $international_map = $this->parseYaml('tv-international.yml');
+
+        $this->station_map = $national_map->merge($network_map)->merge($international_map);
+    }
+
+    protected function parseYaml($file_name)
+    {
+        $res = Yaml::parseFile($this->getFilePath($file_name));
+        return collect($res)->groupBy('raw_name');
+    }
+
+    protected function getFilePath($file_name) 
+    {
+        return dirname(__FILE__) . "/Maps/{$file_name}";
+    }
+}
